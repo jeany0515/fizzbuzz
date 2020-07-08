7794968f66ca816ba5f1a03d3ae703da56c45e58
devxu <devxu@thoughtworks.com>
2020-07-08 10:20:57 +0800

    [FizzBuzz]: refactor for extract private function

--- src/main/java/com/thoughtworks/tdd/CountOff.java
+++ src/main/java/com/thoughtworks/tdd/CountOff.java
@@ -2,15 +2,27 @@ package com.thoughtworks.tdd;
 
 public class CountOff {
     public String count(int order) {
-        if (order%3 == 0) {
+        if (isDivideBy3(order)) {
             return "Fizz";
         }
-        if (order%5 == 0) {
+        if (isDivideBy5(order)) {
             return "Buzz";
         }
-        if (order%7 == 0) {
+        if (isDivideBy7(order)) {
             return "Whizz";
         }
         return String.valueOf(order);
+    }
+
+    private boolean isDivideBy3(int order) {
+        return order%3 == 0;
+    }
+
+    private boolean isDivideBy5(int order) {
+        return order%5 == 0;
+    }
+
+    private boolean isDivideBy7(int order) {
+        return order%7 == 0;
     }
 }
